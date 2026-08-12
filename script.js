2a5b2e9487feaf2162908ad1cf3c0434142caa22
Lucas Piancó <lucaspianco133@gmail.com>
2026-08-11 21:29:04 -0300

    Fazendo o historico, melhorando detalhes do layout

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function limpa_cep(){
 
     id_input.value = ""
     id_resultado.innerText = "";
+    id_resultado.innerHTML = "<br>";
 }
 
 function limpa_endereco(){
@@ -42,8 +43,7 @@ function endereco() {
 
 async function buscar_cep(){
     let cep = document.getElementById("input_cep").value.replace(/[\.-]/g, "");
-    localStorage.setItem('cep',cep);
-    storage_cep = localStorage.getItem('cep');
+    cep_storage = localStorage.setItem("cep",cep);
 
     if(cep.length !== 8){
         let id_resultado = document.getElementById('dados_resultado');
@@ -75,15 +75,67 @@ async function buscar_cep(){
         }
         else{
         id_resultado.innerHTML = `
+            <br>
             <p>Rua: ${dados.logradouro}</p>
             <p>Bairro: ${dados.bairro}</p>
             <p>Cidade: ${dados.localidade}</p>
             <p>Estado: ${dados.estado}</p>
         `;
+
+        let id_div = document.getElementById("dados_historico");
+        let qtd_p = id_div.getElementsByTagName("p").length;
+        let p = document.createElement("p");
+        let cep_armezado = localStorage.getItem('cep');
+        p.textContent = cep_armezado;
+
+        p.onclick = async function(){
+            let input = document.getElementById("input_cep");
+            input.value = cep_armezado ;
+
+            let url = `https://viacep.com.br/ws/${cep_armezado}/json/`;
+            const resposta = await fetch(url);
+
+        if(!resposta.ok){
+            console.log("Verifique o Link");
+        }
+
+        let dados = await resposta.json();
+
+        let id_resultado = document.getElementById('dados_resultado'); 
+        if (dados.erro){
+             id_resultado.innerHTML = `
+            <h4>Resultado</h4>
+            <p>"CEP Invalido, verique seu CEP"</p>
+        `;
+        setTimeout(()=>{
+            id_resultado.innerText ="";
+        },5000)
+        }
+        else{
+        id_resultado.innerHTML = `
+            <br>
+            <p>Rua: ${dados.logradouro}</p>
+            <p>Bairro: ${dados.bairro}</p>
+            <p>Cidade: ${dados.localidade}</p>
+            <p>Estado: ${dados.estado}</p>`
+        };
+    };
+        ultimo_cep = id_div.lastElementChild.textContent;
+        if(cep != ultimo_cep){
+           if(qtd_p >= 4){
+            id_div.removeChild(id_div.firstElementChild);
+             id_div.appendChild(p);
+        }else{
+            id_div.appendChild(p); 
+        }
+        }
+        
+    // E trouxe Efeito viu kakkaakaka oh gloriaaaaaa    
+    // console.log("Me ajude Senhor, Em nome de Jesus, Amém")
     }
     
     }
-  
+   
 
 }
 async function buscar_endereco(){
@@ -97,5 +149,5 @@ let url = `https://viacep.com.br/ws/${estado}/${cidade}/${logradouro}/json/`;
 const resposta = await fetch(url);
 
 
-}
+} 
 
